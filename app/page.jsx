fc45b36ef71cf1049f74d6c80a1001126646c385
Saubhagya-2004 <saubhagyabaliarsingh2@gmail.com>
2026-01-16 21:36:38 +0530

    new

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+'use client'
+import React from 'react'
+const page = () => {
+  return (
+    <div className='bg-yellow-300 w-full h-screen'>
+      Hello
+    </div>
+  )
+}
+
+export default page
